bf22b90c28f61e2d9fcb837ed01d095c4004ba30
sachinspwr <sachin.spwr@outlook.com>
2025-06-01 22:22:57 +0530

    fix domain tenant

--- src/apps/admin-console/pages/tenant/tenant-basic-details.page.tsx
+++ src/apps/admin-console/pages/tenant/tenant-basic-details.page.tsx
@@ -1,19 +1,19 @@
-import { VDynamicForm, VDynamicFormHandle } from "@components/organisms";
-import { FormFieldData, VFormFieldData, VFormFields } from "@types";
-import { TenantsStatus } from "@utils/enums/tenants-status.enum";
-import { Tenant } from "models/tenant/tenant.model";
-import { useMemo, useRef } from "react";
-import { useCreateTenantsMutation, usePatchBasicTenantDetailMutation } from "store/slices/tenants.slice";
-import toast from "react-hot-toast";
-import { mapper } from "mapper";
-import { BasicTenantDetailsRequestDTO } from "@dto/request/basic-tenant-details.dto";
-import { TenantsResponseDTO } from "@dto/response/tenants.response.dto";
-import { VButton } from "@components/atoms";
-import { useNavigate } from "react-router-dom";
+import { VDynamicForm, VDynamicFormHandle } from '@components/organisms';
+import { FormFieldData, VFormFieldData, VFormFields } from '@types';
+import { TenantsStatus } from '@utils/enums/tenants-status.enum';
+import { Tenant } from 'models/tenant/tenant.model';
+import { useMemo, useRef } from 'react';
+import { useCreateTenantsMutation, usePatchBasicTenantDetailMutation } from 'store/slices/tenants.slice';
+import toast from 'react-hot-toast';
+import { mapper } from 'mapper';
+import { BasicTenantDetailsRequestDTO } from '@dto/request/basic-tenant-details.dto';
+import { TenantsResponseDTO } from '@dto/response/tenants.response.dto';
+import { VButton } from '@components/atoms';
+import { useNavigate } from 'react-router-dom';
 
 type TenantBasicDetailsProps = {
   tenant?: Tenant;
-  renderMode: "add" | "edit";
+  renderMode: 'add' | 'edit';
   onComplete: OnCompleteHandler<Tenant>;
 };
 
@@ -32,89 +32,89 @@ function TenantBasicDetails({ tenant, renderMode, onComplete }: TenantBasicDetai
       position: '',
       fields: [
         {
-          name: "name",
-          label: "Tenant Name",
-          type: "text",
-          placeholder: "Enter name",
-          position: "1 1 6",
+          name: 'name',
+          label: 'Tenant Name',
+          type: 'text',
+          placeholder: 'Enter name',
+          position: '1 1 6',
           required: true,
         },
         {
-          name: "slug",
-          type: "text",
-          label: "Slug",
-          placeholder: "Enter Slug",
+          name: 'slug',
+          type: 'text',
+          label: 'Slug',
+          placeholder: 'Enter Slug',
           required: true,
-          position: "1 7 6",
+          position: '1 7 6',
         },
         {
-          name: "email",
-          label: "Email Address",
-          type: "email",
+          name: 'domain',
+          label: 'Domain',
+          type: 'text',
           required: true,
-          placeholder: "Enter your email address",
-          position: "2 1 6",
+          placeholder: 'Enter your domain',
+          position: '2 1 6',
           validate: (value) => {
-            if (value === "") return "Email is required";
+            if (value === '') return 'Email is required';
             const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-            if (!emailRegex.test(value as string)) return "Invalid email format";
+            if (!emailRegex.test(value as string)) return 'Invalid email format';
           },
         },
         {
-          name: "status",
-          label: "Status",
-          type: "select",
+          name: 'status',
+          label: 'Status',
+          type: 'select',
           options: Object.entries(TenantsStatus).map(([key, value]) => ({
             value: key,
             label: value,
           })),
-          placeholder: "Select status",
+          placeholder: 'Select status',
           required: true,
-          position: "2 7 6",
+          position: '2 7 6',
         },
       ],
     },
     {
       type: 'group',
       label: 'Owner Details',
-      position: "4 1 6",
+      position: '4 1 6',
       fields: [
         {
-          name: "firstName",
-          label: "First Name",
-          type: "text",
-          placeholder: "Enter first name",
+          name: 'firstName',
+          label: 'First Name',
+          type: 'text',
+          placeholder: 'Enter first name',
           required: true,
-          position: "5 1 6",
+          position: '5 1 6',
         },
         {
-          name: "lastName",
-          label: "Last Name",
-          type: "text",
-          placeholder: "Enter last name",
+          name: 'lastName',
+          label: 'Last Name',
+          type: 'text',
+          placeholder: 'Enter last name',
           required: true,
-          position: "5 7 6",
+          position: '5 7 6',
         },
         {
-          name: "ownerEmail",
-          label: "Email",
-          type: "email",
-          placeholder: "Enter email",
+          name: 'ownerEmail',
+          label: 'Email',
+          type: 'email',
+          placeholder: 'Enter email',
           required: true,
-          position: "6 1 6",
+          position: '6 1 6',
           validate: (value) => {
-            if (value === "") return "Email is required";
+            if (value === '') return 'Email is required';
             const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-            if (!emailRegex.test(value as string)) return "Invalid email format";
+            if (!emailRegex.test(value as string)) return 'Invalid email format';
           },
         },
         {
-          name: "mobile",
-          label: "Mobile",
-          type: "text",
-          placeholder: "Enter mobile number",
+          name: 'mobile',
+          label: 'Mobile',
+          type: 'text',
+          placeholder: 'Enter mobile number',
           required: true,
-          position: "6 7 6",
+          position: '6 7 6',
         },
       ],
     },
@@ -123,7 +123,12 @@ function TenantBasicDetails({ tenant, renderMode, onComplete }: TenantBasicDetai
       name: 'cancel',
       type: 'custom',
       customContent: (
-        <VButton variant="secondary" onClick={() => { navigate('/admin-console/tenants') }}>
+        <VButton
+          variant="secondary"
+          onClick={() => {
+            navigate('/admin-console/tenants');
+          }}
+        >
           Cancel
         </VButton>
       ),
@@ -156,7 +161,6 @@ function TenantBasicDetails({ tenant, renderMode, onComplete }: TenantBasicDetai
     } as FormFieldData;
   }, [tenant]);
 
-
   const handleFormSubmit = async (formData: VFormFieldData) => {
     try {
       const requestData = {
@@ -174,10 +178,7 @@ function TenantBasicDetails({ tenant, renderMode, onComplete }: TenantBasicDetai
       if (tenant) {
         await patchTenant({ tenantId: tenant.id, tenantData: basicReqData }).unwrap();
         toast.success('Tenant Updated');
-        onComplete(
-          { ...(basicReqData as unknown as Tenant), id: tenant.id },
-          { shouldExit: exitRef.current }
-        );
+        onComplete({ ...(basicReqData as unknown as Tenant), id: tenant.id }, { shouldExit: exitRef.current });
       } else {
         const tenantCreated = await createTenant(basicReqData).unwrap();
         const mappedTenant = mapper.map(tenantCreated, TenantsResponseDTO, Tenant);
@@ -194,7 +195,7 @@ function TenantBasicDetails({ tenant, renderMode, onComplete }: TenantBasicDetai
   return (
     <div className="w-3/4">
       <VDynamicForm
-        key={tenant?.id || "add"}
+        key={tenant?.id || 'add'}
         config={basicDetailConfig}
         onSubmit={handleFormSubmit}
         ref={formRef}
